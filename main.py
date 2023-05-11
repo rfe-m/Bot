9253d6f52b83476c58bedfc54bae48f03754f1c6
Mykhailo Pyrozhenko <insreset@outlook.com>
2023-05-11 18:29:36 +0300

    Init

--- main.py
+++ main.py
@@ -1,26 +1,83 @@
-# This example requires the 'message_content' privileged intents
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 Mykhailo Pyrozhenko @rfe:matrix.org
+#
+# This file is part of the Matrix Moderation Tool.
+#
+# The program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# The program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the program. If not, see <https://www.gnu.org/licenses/>.
 
+import asyncio
+import getpass
+import json
 import os
-import discord
-from discord.ext import commands
+import sys
+
+from nio import LoginResponse
+from scripts.moderator import Moderator
+
+CONFIG_FILE = "credentials.json"
 
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+def write_credentials(resp: LoginResponse, homeserver) -> None:
+    # open the config file in write-mode
+    with open(CONFIG_FILE, "w") as f:
+        # write the login details to disk
+        json.dump(
+            {
+                "homeserver": homeserver,  # e.g. "https://matrix.org"
+                "user_id": resp.user_id,  # e.g. "@user:matrix.org"
+                "device_id": resp.device_id,  # device ID, 10 uppercase letters
+                "access_token": resp.access_token,  # cryptogr. access token
+            },
+            f,
+        )
 
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
+async def main() -> None:
+    # If there are no previously-saved credentials, we'll use the password
+    if not os.path.exists(CONFIG_FILE):
+        print("First time use. Did not find credential file.")
+        homeserver = "https://matrix.example.org"
+        homeserver = input(f"Enter your server address: [{homeserver}] ")
+        user_id = "@user:example.org"
+        user_id = input(f"Enter your full user ID: [{user_id}] ")
+        device_name = "matrix-nio"
+        device_name = input(f"Choose a name for this device: [{device_name}] ")
+        client = Moderator(homeserver, user_id)
+        password = getpass.getpass()
+        try:
+            resp = await client.login(password, device_name)
+        except Exception:
+            print(f"Failed to connect: {homeserver}")
+            sys.exit(1)
+        # check that we logged in succesfully
+        if isinstance(resp, LoginResponse):
+            write_credentials(resp, homeserver)
+        else:
+            print(f"Failed to log in: {resp}")
+            sys.exit(1)
+        print("Logged in using a password. Credentials were stored.")
+    # Otherwise the config file exists, so we'll use the stored credentials
+    else:
+        # open the file in read-only mode
+        with open(CONFIG_FILE, "r") as f:
+            config = json.load(f)
+            client = Moderator(config["homeserver"])
+            client.access_token = config["access_token"]
+            client.user_id = config["user_id"]
+            client.device_id = config["device_id"]
+        print("Logged in using stored credentials.")
+    # Start the bot with 30 millisecond timeout
+    await client.sync_forever(30000)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
-
-
-bot.run(os.environ["DISCORD_TOKEN"])
+asyncio.run(main())
